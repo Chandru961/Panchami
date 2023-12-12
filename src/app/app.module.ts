57c7850cb1ce6ac576e521d0c1fa0e0a583494c1
Chandra Shekar R <rchandru961@gmail.com>
2023-12-12 20:02:00 +0530

    ch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './home/home.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { NavComponent } from './nav/nav.component';
 import { FacilitesComponent } from './facilites/facilites.component';
-import { ThingsToDoComponent } from './things-to-do/things-to-do.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { FooterComponent } from './footer/footer.component';
@@ -21,7 +20,6 @@ import { StandardComponent } from './standard/standard.component';
     RoomsComponent,
     NavComponent,
     FacilitesComponent,
-    ThingsToDoComponent,
     GalleryComponent,
     ContactUsComponent,
     FooterComponent,
